d11ad0b06103de9643246948d1884b69339df09e
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2026-07-11 03:55:49 -0600

    evaluate the rectangular pole winding

--- ArithmeticHodge.lean
+++ ArithmeticHodge.lean
@@ -37,6 +37,7 @@ import ArithmeticHodge.Analysis.PoissonSummation
 import ArithmeticHodge.Analysis.ThetaFunction
 import ArithmeticHodge.Analysis.ZetaFunctionalEquation
 import ArithmeticHodge.Analysis.Contour.ArgumentPrinciple
+import ArithmeticHodge.Analysis.Contour.RectangleWinding
 import ArithmeticHodge.Analysis.WeilDefs
 import ArithmeticHodge.Analysis.MultiplicativeWeil
 import ArithmeticHodge.Analysis.MultiplicativeWeilTranspose
